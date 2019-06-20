250a27be2af2b72d10570197e91a17a2a109d0a4
yogurt_lei <yogurt_lei@foxmail.com>
2019-06-20 14:16:47 +0800

    fix: swagger with request authentication header

--- src/main/java/com/eastrobot/kbs/template/config/Swagger2Config.java
+++ src/main/java/com/eastrobot/kbs/template/config/Swagger2Config.java
@@ -3,37 +3,72 @@
  */
 package com.eastrobot.kbs.template.config;
 
+import com.google.common.collect.Lists;
 import io.swagger.annotations.Api;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import javax.annotation.Resource;
+import java.util.Collections;
+import java.util.List;
+
 /**
- * swagger2 config when active profile is dev or test will show swagger-ui page, otherwise not show
+ * swagger2 config
  *
  * @author Yogurt_lei
  * @date 2019-06-05 11:36
  */
-@Profile({"dev", "test"})
 @Configuration
 @EnableSwagger2
 public class Swagger2Config {
 
+    @Resource
+    private JwtConfig jwtConfig;
+
     @Bean
     public Docket createRestApi() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .produces(Collections.singleton(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .consumes(Collections.singleton(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .ignoredParameterTypes(Authentication.class)
+                .securityContexts(Lists.newArrayList(securityContext()))
+                .securitySchemes(Lists.newArrayList(apiKey()))
+                .useDefaultResponseMessages(false)
                 .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
                 .paths(PathSelectors.any())
                 .build();
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", jwtConfig.getAuthHeader(), "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Lists.newArrayList(new SecurityReference("JWT", authorizationScopes));
     }
 
     private ApiInfo apiInfo() {
