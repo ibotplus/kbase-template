7e0166148fdadd09310dcc73d3c48fdb38c0e330
yogurt_lei <yogurt_lei@foxmail.com>
2019-07-10 17:46:52 +0800

    feat: spring boot admin support

--- src/main/java/com/eastrobot/kbs/template/config/WebSecurityConfig.java
+++ src/main/java/com/eastrobot/kbs/template/config/WebSecurityConfig.java
@@ -62,7 +62,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) {
-        web.ignoring().antMatchers("/v2/api-docs", "/swagger-resources/**", "/swagger-ui.html", "/webjars/**");
+        web.ignoring().antMatchers("/v2/api-docs", "/swagger-resources/**", "/swagger-ui.html", "/webjars/**","/actuator/**", "/ws/**");
     }
 
     @Override
