32c3fac586523d18e5fcb8adea9b5c9a1669ffba
yogurt_lei <yogurt_lei@foxmail.com>
2019-09-02 14:05:03 +0800

    fix: adjust validate exception result

--- src/main/java/com/eastrobot/kbs/template/exception/GlobalExceptionHandler.java
+++ src/main/java/com/eastrobot/kbs/template/exception/GlobalExceptionHandler.java
@@ -4,6 +4,8 @@ import com.eastrobot.kbs.template.model.vo.ResponseEntity;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindException;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -25,9 +27,21 @@ public class GlobalExceptionHandler {
     /**
      * 参数校验异常
      */
-    @ExceptionHandler({ValidationException.class, MethodArgumentNotValidException.class})
+    @ExceptionHandler({ValidationException.class, MethodArgumentNotValidException.class, BindException.class})
     public ResponseEntity validateExceptionHandler(Exception ex) {
         ex.printStackTrace();
+
+        FieldError fieldError = null;
+        if (ex instanceof MethodArgumentNotValidException) {
+            fieldError = ((MethodArgumentNotValidException) ex).getBindingResult().getFieldError();
+        } else if (ex instanceof BindException) {
+            fieldError = ((BindException) ex).getBindingResult().getFieldError();
+        }
+        if (fieldError != null) {
+            log.error(fieldError.getDefaultMessage());
+            return ResponseEntity.failure(ResultCode.PARAMETER_VALIDATE_FAILED, fieldError.getDefaultMessage());
+        }
+
         return ResponseEntity.failure(ResultCode.PARAMETER_VALIDATE_FAILED, ex.getMessage());
     }
 
